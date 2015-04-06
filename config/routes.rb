f4269f55a0c84bf497c32d6a8efa534b37d983d1
Neema <neema122@gmail.com>
2015-04-06 18:16:09 +0000

    user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   resources :topics do
     resources :bookmarks, except: [:index]  do
       resources :likes, only: [:create, :destroy]
   end
 
   devise_for :users
-  resources :users, only: [:show]
+    resources :users, only: [:show]
 
    get 'about' => 'welcome#about'
 
